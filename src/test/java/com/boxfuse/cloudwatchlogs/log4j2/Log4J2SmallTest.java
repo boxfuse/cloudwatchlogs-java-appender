a41d21f0738822844ac81692e3d908be06c2d4cc
Axel Fontaine <business@axelfontaine.com>
2017-03-02 18:11:25 +0100

    Improved tests

--- src/test/java/com/boxfuse/cloudwatchlogs/log4j2/Log4J2SmallTest.java
+++ src/test/java/com/boxfuse/cloudwatchlogs/log4j2/Log4J2SmallTest.java
@@ -4,11 +4,26 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertTrue;
+
 public class Log4J2SmallTest {
+    private static final String MESSAGE = "OK";
+
     @Test
-    public void log() throws InterruptedException {
-        Logger logger = LogManager.getLogger("logback_test");
-        logger.info("OK");
+    public void log() throws Exception {
+        PrintStream original = System.out;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+
+        Logger logger = LogManager.getLogger("log4j2_test");
+        logger.info(MESSAGE);
         Thread.sleep(1000);
+
+        assertTrue(baos.toString("UTF-8").contains(MESSAGE));
+
+        System.setOut(original);
     }
 }
--- src/test/java/com/boxfuse/cloudwatchlogs/logback/LogbackSmallTest.java
+++ src/test/java/com/boxfuse/cloudwatchlogs/logback/LogbackSmallTest.java
@@ -4,11 +4,26 @@ import ch.qos.logback.classic.Logger;
 import org.junit.Test;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertTrue;
+
 public class LogbackSmallTest {
+    private static final String MESSAGE = "OK";
+
     @Test
-    public void log() throws InterruptedException {
+    public void log() throws Exception {
+        PrintStream original = System.out;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+
         Logger logger = (Logger) LoggerFactory.getLogger("logback_test");
-        logger.info("OK");
+        logger.info(MESSAGE);
         Thread.sleep(1000);
+
+        assertTrue(baos.toString("UTF-8").contains(MESSAGE));
+
+        System.setOut(original);
     }
 }
